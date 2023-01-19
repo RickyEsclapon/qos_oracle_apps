81de3ae8bcf42f8ca05596c5d56ca9403cdd8677
ries9112 <89109989+RickyEsclapon@users.noreply.github.com>
2023-01-19 15:44:33 +0000

    test

--- mips/mips.py
+++ mips/mips.py
@@ -86,7 +86,7 @@ def pull_data(nrows):
   min_epoch = 999999999999999999
   
   # Get data for the indexer (30k rows)
-  for i in range(5):
+  for i in range(2):
       if i == 0:
         skip = 0
       else:
